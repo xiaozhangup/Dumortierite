41743b7f958ed01f96b04f8b25e9ec62bd55c4b3
Sunshine-wzy <sunshinewzy@qq.com>
2023-05-08 20:16:02 +0800

    Fix the bug of override final functions by adding DumoSlimefunAddon

--- src/main/kotlin/io/sn/dumortierite/DumoCore.kt
+++ src/main/kotlin/io/sn/dumortierite/DumoCore.kt
@@ -1,28 +1,17 @@
 package io.sn.dumortierite
 
-import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon
 import org.bukkit.plugin.java.JavaPlugin
-import java.util.logging.Logger
 
-class DumoCore : SlimefunAddon, JavaPlugin() {
+class DumoCore : JavaPlugin(), DumoSlimefunAddon {
 
     override fun getJavaPlugin(): JavaPlugin {
         return this
     }
 
-    override fun getBugTrackerURL(): String? {
+    override fun getBugTrackerURL(): String {
         return "https://github.com/freeze-dolphin/Dumortierite/issues"
     }
 
-/*
-    override fun getName(): String {
-        return super<JavaPlugin>.getName()
-    }
-*/
-
-    override fun getLogger(): Logger {
-        return getLogger()
-    }
 
     override fun onEnable() {
         TODO()
